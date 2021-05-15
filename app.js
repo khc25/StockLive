6ffe4df471f346c60794d445afaa7d227731e6c6
Brian Chow <32953270+khc25@users.noreply.github.com>
2021-05-15 22:46:36 +0800

    update

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const { Pool } = require('pg')
+
+// user: 'brian',
+// password: '12345',
+// host: 'localhost',
+// database: 'stock',
+// port: 5432,
+
+const connectionString = 'postgres://xrwgvakl:12eZbGMSQmVSdEyDvhxq34R4mP-FpWB3@queenie.db.elephantsql.com:5432/xrwgvakl'
 const pool  = new Pool({
-  user: 'brian',
-  password: '12345',
-  host: 'localhost',
-  database: 'stock',
-  port: 5432,
+  connectionString : connectionString,
 })
 const WebSocket = require('ws')
 const socket = new WebSocket('wss://ws.finnhub.io?token=c20n99qad3if82u49o10');
 
 // Connection opened -> Subscribe
 socket.addEventListener('open', function (event) {
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'TSLA'}))
-    // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+    // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'TSLA'}))
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
     // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
 });
 
@@ -26,7 +30,7 @@ socket.addEventListener('message', function (event) {
       var d = new Date();
       var date = d.getDate();
       console.log(data);
-      pool.query("INSERT INTO TSLA (d,c,p,s,t,v) VALUES ($1, $2, $3, $4, $5, $6)RETURNING id",[date,data.c,data.p,data.s,data.t,data.v], (err,res) => {
+      pool.query("INSERT INTO bitcoin (d,c,p,s,t,v) VALUES ($1, $2, $3, $4, $5, $6)RETURNING id",[date,data.c,data.p,data.s,data.t,data.v], (err,res) => {
         if (err) {
           console.log(err);
         } else {
@@ -37,7 +41,7 @@ socket.addEventListener('message', function (event) {
 
     }
     catch(err) {
-      throw err;
+      console.log(err);
     }
 
 });
---
+++ createTable.js
@@ -0,0 +1,3 @@
+
+
+CREATE TABLE bitcoin (id serial primary key, d int,c varchar ,p double precision,s varchar,t BIGINT,v double precision)
